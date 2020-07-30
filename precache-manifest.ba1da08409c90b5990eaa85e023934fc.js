8577392d8952d4b9470ae6ce82bb6fd4faffeede
jeffreylaw <jylday56@gmail.com>
2020-07-29 21:30:56 -0700

    Updates

---
+++ precache-manifest.ba1da08409c90b5990eaa85e023934fc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c5e4fe64e0c3b0cac685d0d92f8552",
+    "url": "/jeffreylaw/lets-eat/index.html"
+  },
+  {
+    "revision": "7cdeb359d3dc85546b3b",
+    "url": "/jeffreylaw/lets-eat/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "140363b2a88a87247d7a",
+    "url": "/jeffreylaw/lets-eat/static/css/main.a30e685c.chunk.css"
+  },
+  {
+    "revision": "7cdeb359d3dc85546b3b",
+    "url": "/jeffreylaw/lets-eat/static/js/2.ef4aaf3b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/jeffreylaw/lets-eat/static/js/2.ef4aaf3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "140363b2a88a87247d7a",
+    "url": "/jeffreylaw/lets-eat/static/js/main.ba35df38.chunk.js"
+  },
+  {
+    "revision": "c9ac9a31d28bb64e5a86",
+    "url": "/jeffreylaw/lets-eat/static/js/runtime-main.f35558af.js"
+  },
+  {
+    "revision": "12bc02f14de8806e9f8daf63a58c084b",
+    "url": "/jeffreylaw/lets-eat/static/media/cute.12bc02f1.svg"
+  },
+  {
+    "revision": "fb0434cc7fb041f58722269fcf14efa7",
+    "url": "/jeffreylaw/lets-eat/static/media/dishes.fb0434cc.svg"
+  },
+  {
+    "revision": "608ab5fe4a45e2ac93febd3416b260f0",
+    "url": "/jeffreylaw/lets-eat/static/media/dust_scratches.608ab5fe.png"
+  },
+  {
+    "revision": "7e62e439a81332a3953c29c62f286afe",
+    "url": "/jeffreylaw/lets-eat/static/media/non_existent_image2.7e62e439.jpg"
+  }
+]);
