df71a2cea19ac45be4b699b003d00444a9369213
Jeffrey Law <jylday56@gmail.com>
2022-02-03 21:19:11 -0800

    Remove build/ from gitignore

---
+++ build/precache-manifest.1990b98ca712f7d4aabd734f9bdca313.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab474cfbc625f6dc6e8b525002db233",
+    "url": "/lets-eat/index.html"
+  },
+  {
+    "revision": "cab7ae4b3a10c6cd8e8a",
+    "url": "/lets-eat/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "3686e37f58400faad1f9",
+    "url": "/lets-eat/static/css/main.e5d64770.chunk.css"
+  },
+  {
+    "revision": "cab7ae4b3a10c6cd8e8a",
+    "url": "/lets-eat/static/js/2.030733cf.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/lets-eat/static/js/2.030733cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3686e37f58400faad1f9",
+    "url": "/lets-eat/static/js/main.b0210dcb.chunk.js"
+  },
+  {
+    "revision": "a5256494d3ff318c0c43",
+    "url": "/lets-eat/static/js/runtime-main.b9e3d6f9.js"
+  },
+  {
+    "revision": "bf62ce10d9b1427d7155f3435baa2920",
+    "url": "/lets-eat/static/media/bg.bf62ce10.jpg"
+  },
+  {
+    "revision": "12bc02f14de8806e9f8daf63a58c084b",
+    "url": "/lets-eat/static/media/cute.12bc02f1.svg"
+  },
+  {
+    "revision": "fb0434cc7fb041f58722269fcf14efa7",
+    "url": "/lets-eat/static/media/dishes.fb0434cc.svg"
+  },
+  {
+    "revision": "7e62e439a81332a3953c29c62f286afe",
+    "url": "/lets-eat/static/media/non_existent_image2.7e62e439.jpg"
+  }
+]);
